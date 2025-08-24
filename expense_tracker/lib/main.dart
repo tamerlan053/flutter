b118c3cb6923024212b6ad6c195fbbbe1f672385
tamerlan <tamerlan.aslakhanov@gmail.com>
2025-08-24 23:09:33 +0200

    Expense Tracker - New expense functionality implemented

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,7 +1,38 @@
 import 'package:flutter/material.dart';
 
-import 'package:expense_tracker/expenses.dart';
+import 'package:expense_tracker/widgets/expenses.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181),
+);
 
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
+      home: const Expenses(),
+    ),
+  );
 }
